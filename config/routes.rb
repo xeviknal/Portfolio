5fcb8aef7ba663692f0d00e4271f15bef5efe874
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-21 15:51:55 +0100

    Friendly urls applied to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Portfolio::Application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    get "sign_in", :to => "devise/sessions#new"
-    get "sign_out", :to => "devise/sessions#destroy"
-    get "sign_up", :to => "devise/registrations#new"
-    get "forgot_password", :to => "devise/passwords#new"	
+  devise_for :users do
+    get "sign_in", :to => "devise/sessions#new", :as => :new_user_session
+    get "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session
+    get "sign_up", :to => "devise/registrations#new", :as => :new_user_registration
+    get "forgot_password", :to => "devise/passwords#new", :as => :new_user_password
   end
 
   root :to => 'static#index'
