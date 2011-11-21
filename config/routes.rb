b24dbb46e3ab48082fcf8fd6d6c01bb5a501a615
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-21 18:00:54 +0100

    Fixing routes for sign_in and sign_out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Portfolio::Application.routes.draw do
-  devise_for :users do
+  devise_for :users , :controllers => {:sessions => 'custom_devise/sessions'}, :skip => [:sessions] do
     get "sign_in", :to => "devise/sessions#new", :as => :new_user_session
-    get "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session
+    post "sign_in", :to => "devise/sessions#create", :as => :user_session
+    delete "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session
     get "sign_up", :to => "devise/registrations#new", :as => :new_user_registration
+    get "edit_profile", :to => "devise/registrations#edit", :as => :edit_user_registration
     get "forgot_password", :to => "devise/passwords#new", :as => :new_user_password
   end
 
