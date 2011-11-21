583fe23720285088a0c69538335f2828a42d6ede
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-21 11:03:16 +0100

    Intial add Rails app and delete readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '15ab4e4824e60361cff21294e2c217a22793f0571a6f4578bc4d53c5fdb9050a46ad6652ea86707930c6219b97d60cf7ab35540268c62b3ed6650c7ade635b76'
