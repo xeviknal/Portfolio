4a88662b5ef808171dc057455b8fb1a53832bfec
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-22 17:11:52 +0100

    CRUD added for admin

--- app/controllers/admin/designs_controller.rb
+++ app/controllers/admin/designs_controller.rb
@@ -1,6 +1,45 @@
 class Admin::DesignsController < Admin::AdminController
 
+  def index
+    @designs = Design.all
+  end
+
   def show
     @design = Design.find(params[:id])
+  end
+
+  def new
+    @design = Design.new
+  end
+
+  def edit
+    @design = Design.find(params[:id])
+  end
+
+  def create
+    @design = Design.new(params[:design])
+
+    if @design.save
+      redirect_to( admin_design_path(@design), :notice => 'Post was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    @design = Design.find(params[:id])
+
+    if @design.update_attributes(params[:design])
+      redirect_to admin_design_path(@design), notice: 'Design was successfully updated'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @design = Design.find(params[:id])
+    @design.destroy
+
+    redirect_to admin_designs_url
   end
 end
