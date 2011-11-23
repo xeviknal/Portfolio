8dc0df1003aecf16211d5adb4bba7be30233bb64
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-23 11:34:08 +0100

    Remove thumb_image attr from Design table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111122094458) do
+ActiveRecord::Schema.define(:version => 20111123102950) do
 
   create_table "designs", :force => true do |t|
     t.string   "title"
