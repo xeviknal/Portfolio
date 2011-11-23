439b12d1f9ccc7e63acf1be3b8d373ea64a63462
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-23 11:22:47 +0100

    Adding thumbs to view, whilelist to uploader

--- app/controllers/admin/designs_controller.rb
+++ app/controllers/admin/designs_controller.rb
@@ -18,9 +18,8 @@ class Admin::DesignsController < Admin::AdminController
 
   def create
     @design = Design.new(params[:design])
-
     if @design.save
-      redirect_to( admin_design_path(@design), :notice => 'Post was successfully created.')
+      redirect_to( admin_design_path(@design), :notice => 'Design was successfully created.')
     else
       render :action => "new"
     end
@@ -28,7 +27,7 @@ class Admin::DesignsController < Admin::AdminController
 
   def update
     @design = Design.find(params[:id])
-
+    
     if @design.update_attributes(params[:design])
       redirect_to admin_design_path(@design), notice: 'Design was successfully updated'
     else
