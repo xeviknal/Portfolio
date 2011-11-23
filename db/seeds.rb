a19178b225c6fe1e8853f8a21dbd86efa21a8ed3
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-23 17:25:52 +0100

    Creating seeds data for designs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Design.delete_all
+PICTURE_BASE = '/app/assets/images/test/'
+30.times do |i|
+  Design.create :title => "Design ##{i+1}", :description => "Description for the design ##{i+1}", :image => File.open(File.join(Rails.root, PICTURE_BASE + "description.jpg"))
+end
