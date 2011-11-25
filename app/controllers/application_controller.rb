acec5b81db461bf76d19dbf3bf3673f0d029202a
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-25 16:17:10 +0100

    Adding multilanguage in catalan and english. Missing devise translate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+  before_filter :check_locale
+
+  rescue_from Exception, :with => :handle_exceptions
+
+  def handle_exceptions(e)
+    case e
+    #handling exception of ActionController::RoutingError
+    when ActionController::RoutingError
+      redirect_to "/404.html"
+    end
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def check_locale
+    if params[:locale].blank? || I18n.available_locales.select{ |l| l == params[:locale].to_sym }.blank?
+      raise ActionController::RoutingError, "invalid route"
+    end
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    options.merge( { :locale => I18n.locale.to_sym } )
+  end
 end
