1a36d9268f8f0a3c382c09643f2953bd3d8c2896
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-11-29 13:53:42 +0100

    Adding validations for association images on Design and sort_by for images

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,10 +1,15 @@
 class Design < ActiveRecord::Base
   has_many :images, :dependent => :destroy
   validates_presence_of :title, :description
+  validate :has_images?
   accepts_nested_attributes_for :images, :allow_destroy => true
 
   #per_page constant for will_paginate gem
   self.per_page = 10
+
+  def has_images?
+    self.errors.add :base, "At least you must create one image" if self.images.blank?
+  end
 
   def disqus_identificator
     "#{Digest::MD5.hexdigest id.to_s}"
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ActiveRecord::Base
   mount_uploader :image, PictureUploader
   belongs_to :design
+
   validates_presence_of :image
+  validates_numericality_of :sort_by
 end
