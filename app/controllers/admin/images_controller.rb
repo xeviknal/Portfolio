98431829a4e2c65c0731e855b931520c0934b559
Xavier Canal i Masjuan <xeviknal@gmail.com>
2011-12-01 11:33:58 +0100

    Removed unnecessary format.html for update action.

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -4,10 +4,8 @@ class Admin::ImagesController < Admin::AdminController
     @image = Image.find(params[:id])
     respond_to do |format|
       if @image.update_attributes(params[:image])
-        format.html { redirect_to @desing, notice: 'Image was successfully updated.' }
         format.json { render json: {:thumb => @image.image_url(:thumb), :original => @image.image_url } }
       else
-        format.html { render action: "edit" }
         format.json { render json: @image.errors, status: :unprocessable_entity }
       end
     end
